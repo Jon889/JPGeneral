6b436d6a73bb50ff33dfff7d6eecb490cdfdb99e
Jonathan Bailey <jon.bailey889@gmail.com>
2011-02-15 18:16:20 +0000

    Changed method name to replace "andAlpha" with "A"
    Fits more inline with Apple method name.

--- UIColor+JPExtras.h
+++ UIColor+JPExtras.h
@@ -2,5 +2,5 @@
  Copyright (c) 2011 Jonathan Bailey
  May be modified and used in any project as long as all comments, including this one, remain.*/
 @interface UIColor (JPExtras)
-+ (UIColor *)colorWithR:(CGFloat)red G:(CGFloat)green B:(CGFloat)blue andAlpha:(CGFloat)alpha;
++ (UIColor *)colorWithR:(CGFloat)red G:(CGFloat)green B:(CGFloat)blue A:(CGFloat)alpha;
 @end
--- UIColor+JPExtras.m
+++ UIColor+JPExtras.m
@@ -2,7 +2,7 @@
  Copyright (c) 2011 Jonathan Bailey
  May be modified and used in any project as long as all comments, including this one, remain.*/
 @implementation UIColor (JPExtras)
-+ (UIColor *)colorWithR:(CGFloat)red G:(CGFloat)green B:(CGFloat)blue andAlpha:(CGFloat)alpha {
++ (UIColor *)colorWithR:(CGFloat)red G:(CGFloat)green B:(CGFloat)blue A:(CGFloat)alpha {
 	return [UIColor colorWithRed:(red/255.0) green:(green/255.0) blue:(blue/255.0) alpha:alpha];
 }
 @end

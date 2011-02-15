0a746150336ae2c63a439b09ca84f9601793bd5c
Jonathan Bailey <jon.bailey889@gmail.com>
2011-02-15 18:07:22 +0000

    Added JPGeneralFunctions.h
    Radians <> Degrees conversion

---
+++ JPGeneralFunctions.h
@@ -0,0 +1,12 @@
+/* Part of JPGeneral repository, Jon889: https://github.com/Jon889/JPGeneral
+ Copyright (c) 2011 Jonathan Bailey
+ May be modified and used in any project as long as all comments, including this one, remain.*/
+CG_INLINE CGFloat DegToRad(CGFloat degrees)
+{
+	return degrees * M_PI / 180;
+};
+
+CG_INLINE CGFloat RadToDeg(CGFloat radians)
+{
+	return radians * 180 / M_PI;
+};

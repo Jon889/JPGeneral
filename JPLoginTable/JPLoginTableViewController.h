f1de2155fc5cf9e778a117e81f00dc126ac2c01a
Jonathan Bailey <jon.bailey889@gmail.com>
2011-02-21 12:10:06 +0000

    Added Login table view controller
    Based on the facebook login page, using an UITableView

---
+++ JPLoginTable/JPLoginTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  JPLoginTableViewController.h
+//  JPLoginTableView
+//
+//  Created by Jonathan on 21/02/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JPLoginTableViewController : UITableViewController {
+
+}
+
+@end
+
